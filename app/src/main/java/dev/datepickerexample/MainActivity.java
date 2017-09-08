c44f19c9286df6f4d5c46edc148b7418259986b9
devashishkatoriya <devashishkatoriya@gmail.com>
2017-09-08 19:36:57 +0530

    v1

---
+++ app/src/main/java/dev/datepickerexample/MainActivity.java
@@ -0,0 +1,20 @@
+package dev.datepickerexample;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+}
